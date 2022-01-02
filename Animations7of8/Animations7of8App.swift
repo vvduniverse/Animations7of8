d6bc90976dc62f30a52e8d454115488334a63656
Vahtee Boo <ronnie.kaa@bigmir.net>
2022-01-02 16:31:06 +0200

    Initial Commit

---
+++ Animations7of8/Animations7of8App.swift
@@ -0,0 +1,17 @@
+//
+//  Animations7of8App.swift
+//  Animations7of8
+//
+//  Created by Vahtee Boo on 02.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct Animations7of8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
