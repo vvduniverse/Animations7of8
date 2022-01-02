9e5e654bdccb05f2c41214b6ef77f94027294c69
Vahtee Boo <ronnie.kaa@bigmir.net>
2022-01-02 16:53:56 +0200

    part 7 of 8

--- Animations7of8/ContentView.swift
+++ Animations7of8/ContentView.swift
@@ -8,9 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isShowingRed = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Button("Tap Me") {
+                withAnimation {
+                    isShowingRed.toggle()
+                }
+            }
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+            }
+        }
     }
 }
 
